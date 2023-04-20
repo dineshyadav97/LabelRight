6d6e37d8d9a50580a765f75f9a4ea9c7b76cc922
Prachi Gaikwad <prachi.gaikwad@tigeranalytics.local>
2023-04-20 20:50:05 +0530

    Updated Interrupted flows

--- src/test/java/test/LabelRightAutomationTest.java
+++ src/test/java/test/LabelRightAutomationTest.java
@@ -13,8 +13,8 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFSheet;  
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -30,14 +30,12 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-
 public class LabelRightAutomationTest {
-	
+
 	private static WebDriver driver;
 	private static Logger log = LogManager.getLogger();
 	private static JavascriptExecutor js;
-	
-	
+
 	private static Authentication authentication;
 	private static ArtworkManualFlows artworkManualFlows;
 	private static DashboardFlow dashboardFlow;
@@ -48,15 +46,13 @@ public class LabelRightAutomationTest {
 	private static ArtworkAndLidInterruption artworkAndLidInterruption;
 	private static ResultInterruptionFlow resultInterruptionFlow;
 	private static ResultInterruption resultInterruption;
-	
 
-	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		
-		//Starting the Application
-		System.setProperty("webdriver.chrome.driver",Constants.WebDriverUrl );
-		driver= new ChromeDriver();
+
+		// Starting the Application
+		System.setProperty("webdriver.chrome.driver", Constants.WebDriverUrl);
+		driver = new ChromeDriver();
 		js = (JavascriptExecutor) driver;
 		driver.get(Constants.loggedInUrl);
 		driver.manage().window().maximize();
@@ -71,131 +67,134 @@ public class LabelRightAutomationTest {
 		artworkAndLidInterruption = new ArtworkAndLidInterruption(driver);
 		resultInterruptionFlow = new ResultInterruptionFlow(driver);
 		resultInterruption = new ResultInterruption(driver);
-		
+
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
-		//Closing the Application
+		// Closing the Application
 		log.info("Application Closed");
 		driver.quit();
 	}
 
 	@Before
 	public void setUp() throws Exception {
-		
-		//Login
+
+		// Login
 		log.info("Login Process Started.");
 		Authentication.Login();
-		
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		
-		//Logout
+
+		// Logout
 		log.info("Logout Process Started.");
 		Authentication.Logout();
 	}
 
 	@Test
 	public void test1() throws InterruptedException, IOException {
-		
+
 		FileInputStream artworkRequestsFile = new FileInputStream(Constants.ArtworkRequests);
-		
+
 		XSSFWorkbook artworkRequestWB = new XSSFWorkbook(artworkRequestsFile);
 		XSSFSheet sheet = artworkRequestWB.getSheet("Sheet1");
-		Iterator<Row> itr = sheet.iterator();   
+		Iterator<Row> itr = sheet.iterator();
 		itr.next(); // not taking the header
-		while (itr.hasNext())                 
-		{  
-		Row row = itr.next();  
-		Iterator<Cell> cellIterator = row.cellIterator();    
-		
-		Cell artworkUrlCell = row.getCell(2);
-		String artworkUrl = artworkUrlCell.getStringCellValue(); 
-		Cell lidUrlCell = row.getCell(3);
-		String lidUrl = lidUrlCell.getStringCellValue(); 
-		Cell regionCategoryCell = row.getCell(0);
-		String regionCategory = regionCategoryCell.getStringCellValue(); 
-		Cell packageTypeCell = row.getCell(1);
-		String packageType = packageTypeCell.getStringCellValue();
-		
-		RegionCategoryPackageType regionCategoryPackageType  = artworkManualFlows.convertToOptions(regionCategory,packageType);
-		
-		log.info("Adhoc Flow Started.");
-		ArtworkDto artworkDto = artworkManualFlows.Flow1(regionCategoryPackageType.getRegionCategory(),regionCategoryPackageType.getPackageType(),artworkUrl,lidUrl);
-		log.info(artworkDto.getArtworkID()+" "+artworkDto.getErrorCount());
-		
-        WebElement start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
-		js.executeScript("arguments[0].click();", start_page);
+		while (itr.hasNext()) {
+			Row row = itr.next();
+			Iterator<Cell> cellIterator = row.cellIterator();
 
-		log.info("Checking if error count in results page is equal to error count in reviewed page.");
-		artworkManualFlows.checkErrorCount(artworkDto,regionCategoryPackageType.getRegionCategory());
-		
-		start_page = driver.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
-		js.executeScript("arguments[0].click();", start_page);
+			Cell artworkUrlCell = row.getCell(2);
+			String artworkUrl = artworkUrlCell.getStringCellValue();
+			Cell lidUrlCell = row.getCell(3);
+			String lidUrl = lidUrlCell.getStringCellValue();
+			Cell regionCategoryCell = row.getCell(0);
+			String regionCategory = regionCategoryCell.getStringCellValue();
+			Cell packageTypeCell = row.getCell(1);
+			String packageType = packageTypeCell.getStringCellValue();
 
-		
+			RegionCategoryPackageType regionCategoryPackageType = artworkManualFlows.convertToOptions(regionCategory,
+					packageType);
+
+			log.info("Adhoc Flow Started.");
+			ArtworkDto artworkDto = artworkManualFlows.Flow1(regionCategoryPackageType.getRegionCategory(),
+					regionCategoryPackageType.getPackageType(), artworkUrl, lidUrl);
+			log.info(artworkDto.getArtworkID() + " " + artworkDto.getErrorCount());
+
+			WebElement start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
+			js.executeScript("arguments[0].click();", start_page);
+
+			log.info("Checking if error count in results page is equal to error count in reviewed page.");
+			artworkManualFlows.checkErrorCount(artworkDto, regionCategoryPackageType.getRegionCategory());
+
+			start_page = driver
+					.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
+			js.executeScript("arguments[0].click();", start_page);
+
 		}
-		
-	
+
 		WebElement start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-		
 
 		log.info("Dashboard Flow - Review In Progress Started.");
 		dashboardFlow.ReviewInProgress();
-		
-		start_page = driver.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
+
+		start_page = driver
+				.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-		
+
 		log.info("Dashboard Flow - Reviewed Started.");
 		dashboardFlow.Reviewed();
-		
-		start_page = driver.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
+
+		start_page = driver
+				.findElement(By.xpath("/html/body/app-root/main/app-homepage/div/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-		
+
 		log.info("Dashboard Flow - Review In Progress - Resume Started.");
 		resumeFunctionality.resumeFlow();
-		
+
 		log.info("Artwork interrupt flow started");
-		artworkInterrupt.artworkInterruptFlow(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
-		
+		artworkInterrupt.artworkInterruptFlow(Constants.RegionCategory_US_SocialBeverages, Constants.Package_Primary,
+				Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 		start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
-		js.executeScript("arguments[0].click();", start_page); 
-	
+		js.executeScript("arguments[0].click();", start_page);
+
 		log.info("verify interrupt flow started");
-		verifyInterruptionFlow.Flow5(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
-		
+		verifyInterruptionFlow.Flow5(Constants.RegionCategory_US_SocialBeverages, Constants.Package_Primary,
+				Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 		start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-	
+
 		log.info("Flow3 Started.");
-		LidInteruptedFlow.Flow3(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl,Constants.Flow1LidUrl);
-		
+		LidInteruptedFlow.Flow3(Constants.RegionCategory_US_SocialBeverages, Constants.Package_Primary,
+				Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 		start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-	
+
 		log.info("Flow4 Started.");
-		artworkAndLidInterruption.Flow4(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl,Constants.Flow1LidUrl);
-			
+		artworkAndLidInterruption.Flow4(Constants.RegionCategory_US_SocialBeverages, Constants.Package_Primary,
+				Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 		start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
-		
+
 		log.info("Results interrupt flow started");
-		resultInterruption.resultInterruptionFlow(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
-		
+		resultInterruption.resultInterruptionFlow(Constants.RegionCategory_US_SocialBeverages,
+				Constants.Package_Primary, Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 		start_page = driver.findElement(By.xpath("/html/body/app-root/header/app-header/div/h3/span"));
 		js.executeScript("arguments[0].click();", start_page);
 
 		log.info("Flow6 Started.");
-		resultInterruptionFlow.Flow6(Constants.RegionCategory_US_SocialBeverages,Constants.Package_Primary,Constants.Flow1ArtworkUrl,Constants.Flow1LidUrl);
-		
-		
+		resultInterruptionFlow.Flow6(Constants.RegionCategory_US_SocialBeverages, Constants.Package_Primary,
+				Constants.Flow1ArtworkUrl, Constants.Flow1LidUrl);
+
 	}
-	
-	
-	
 
 }
